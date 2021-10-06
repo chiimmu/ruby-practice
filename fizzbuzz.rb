bf922d150160f2f8a8c2b9558003f65a7d543327
今井賢人 <kento@imaikenjinnoMacBook-Air.local>
2021-10-06 19:07:44 +0900

    fizzbuzz fixed

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,16 @@
-(1..30).each do |i|
+input = gets
+num = input.to_i
+
+(1..num).each do |i|
   
-   if i%15==0
-     puts "FizzBuzz!"
-   elsif i%3==0
-     puts "Fizz!"
-   elsif i%5==0 
-     puts "Buzz!"
-   else
-     puts i
-   end
+  if i%15 == 0
+    puts "FizzBuzz!"
+  elsif i%3 == 0
+    puts "Fizz!"
+  elsif i%5 == 0  
+    puts "Buzz!"
+  else
+    puts i
+  end
 
 end
