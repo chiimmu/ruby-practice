d68ad082a63cb6fca83684400640dc6861b2e599
今井賢人 <kento@imaikenjinnoMacBook-Air.local>
2021-10-16 13:29:47 +0900

    ruby bubblesort indent fixed

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,15 +1,15 @@
 array = [10, 8, 3, 5, 2, 4, 11, 18, 20, 33]
 
 len = array.length
-  (1..len).each do |i|
-    (1..(len - i)).each do |j|
-      maenokazu = j - 1
-      if array[maenokazu] > array[j]
-        save = array[maenokazu]
-        array[maenokazu] = array[j]
-        array[j] = save
-        p array
-      end
+(1..len).each do |i|
+  (1..(len - i)).each do |j|
+    maenokazu = j - 1
+    if array[maenokazu] > array[j]
+      save = array[maenokazu]
+      array[maenokazu] = array[j]
+      array[j] = save
+      p array
     end
   end
+end
   
